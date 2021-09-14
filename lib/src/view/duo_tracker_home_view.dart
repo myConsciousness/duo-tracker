5ea301a88f2142bac5dcd8c252b42707ebcf3cb0
Kato Shinya <kato.shinya.dev@gmail.com>
2021-09-14 19:46:09 +0900

    update

--- lib/src/view/duo_tracker_home_view.dart
+++ lib/src/view/duo_tracker_home_view.dart
@@ -8,6 +8,7 @@ import 'package:duo_tracker/src/view/settings/settings_view.dart';
 import 'package:duo_tracker/src/view/statistics/statistics_view.dart';
 import 'package:duo_tracker/src/view/user/user_account_view.dart';
 import 'package:flutter/material.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 
 class DuoTrackerHomeView extends StatefulWidget {
   const DuoTrackerHomeView({Key? key}) : super(key: key);
@@ -49,10 +50,27 @@ class _DuoTrackerHomeViewState extends State<DuoTrackerHomeView> {
               ),
             ),
             tabs: const [
-              Tab(icon: Icon(Icons.list_alt)),
-              Tab(icon: Icon(Icons.bar_chart)),
-              Tab(icon: Icon(Icons.account_circle)),
-              Tab(icon: Icon(Icons.settings)),
+              Tab(
+                icon: Icon(
+                  FontAwesomeIcons.listAlt,
+                  size: 16,
+                ),
+              ),
+              Tab(
+                icon: Icon(
+                  FontAwesomeIcons.chartPie,
+                  size: 16,
+                ),
+              ),
+              Tab(
+                icon: Icon(
+                  FontAwesomeIcons.userAlt,
+                  size: 16,
+                ),
+              ),
+              Tab(
+                icon: Icon(Icons.settings),
+              ),
             ],
           ),
           body: const TabBarView(
