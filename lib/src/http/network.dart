e0ca2405a1206967548c680e4faeb89239272ece
Kato Shinya <kato.shinya.dev@gmail.com>
2021-10-16 10:07:31 +0900

    update

--- lib/src/http/network.dart
+++ lib/src/http/network.dart
@@ -2,7 +2,7 @@
 // Use of this source code is governed by a
 // BSD-style license that can be found in the LICENSE file.
 
-import 'package:connectivity/connectivity.dart';
+import 'package:connectivity_plus/connectivity_plus.dart';
 
 class Network {
   static Future<bool> isConnected() async {
